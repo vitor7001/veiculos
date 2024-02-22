b4fe184fe3c8849c88c2a4b04905536a473281fc
vitor7001 <vitorgomes7001@gmail.com>
2024-02-21 23:01:10 -0300

    feat: adicionando métodos para deletar, atualizar e buscar por id

--- veiculos/src/main/java/com/gerenciador/veiculos/Main.java
+++ veiculos/src/main/java/com/gerenciador/veiculos/Main.java
@@ -1,10 +1,18 @@
 package com.gerenciador.veiculos;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Main {
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(Main.class, args);
--- veiculos/src/main/java/com/gerenciador/veiculos/api/resource/VeiculoController.java
+++ veiculos/src/main/java/com/gerenciador/veiculos/api/resource/VeiculoController.java
@@ -4,8 +4,10 @@ import com.gerenciador.veiculos.api.dto.VeiculoDTO;
 import com.gerenciador.veiculos.model.Veiculo;
 import com.gerenciador.veiculos.service.VeiculoService;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.Valid;
 
@@ -14,16 +16,59 @@ import javax.validation.Valid;
 public class VeiculoController {
 
     private VeiculoService veiculoService;
+    private final ModelMapper modelMapper;
 
-    public VeiculoController(VeiculoService veiculoService) {
+
+    public VeiculoController(VeiculoService veiculoService, ModelMapper modelMapper) {
         this.veiculoService = veiculoService;
+        this.modelMapper = modelMapper;
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public VeiculoDTO criar(@RequestBody @Valid Veiculo dto){
 
+        Veiculo veiculo = modelMapper.map(dto, Veiculo.class);
+
+        veiculo = veiculoService.salvar(veiculo);
+
         return new VeiculoDTO();
+    }
+
+    @GetMapping("{id}")
+    public VeiculoDTO buscar(@PathVariable Long id){
+
+        return veiculoService.buscarPorId(id).map(veiculo -> modelMapper.map(veiculo, VeiculoDTO.class))
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+    }
+
+    @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deletar(@PathVariable Long id){
+
+        Veiculo veiculo = veiculoService.buscarPorId(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+        veiculoService.excluir(veiculo);
+    }
+
+    @PutMapping("{id}")
+    public VeiculoDTO atualizar(@PathVariable Long id, @RequestBody @Valid VeiculoDTO dto){
+
+        Veiculo veiculo = veiculoService.buscarPorId(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+        veiculo.setChassi(dto.getChassi());
+        veiculo.setName(dto.getName());
+        veiculo.setManufacturer(dto.getManufacturer());
+        veiculo.setYear(dto.getYear());
+        veiculo.setColor(dto.getColor());
+        veiculo.setStatus(dto.getStatus());
+        veiculo.setPlaca(dto.getPlaca());
+
+        veiculo = veiculoService.atualizar(veiculo);
+
+        return modelMapper.map(veiculo, VeiculoDTO.class);
     }
 
 }
